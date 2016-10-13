bcd7cf8ae842e16465eeb9fb7c566bdeab4a384b
Jules LaPrairie <julesl@pksound.ca>
2016-10-13 11:36:27 -0600

    Location3D is now c# 4 compliant

--- source/Pk.Spatial/Location3D.cs
+++ source/Pk.Spatial/Location3D.cs
@@ -3,29 +3,29 @@ using UnitsNet;
 
 namespace Pk.Spatial
 {
-	/// <summary>
-	///   Describes a location in 3D space.
-	///   X Y and Z each describe the distance from the origin in their respective axis.
-	/// </summary>
-	public struct Location3D
-	{
-		public Location3D(Length x, Length y, Length z)
-		{
-			this.X = x;
-			this.Y = y;
-			this.Z = z;
-		}
+  /// <summary>
+  ///   Describes a location in 3D space.
+  ///   X Y and Z each describe the distance from the origin in their respective axis.
+  /// </summary>
+  public struct Location3D
+  {
+    public Location3D(Length x, Length y, Length z) : this()
+    {
+      this.X = x;
+      this.Y = y;
+      this.Z = z;
+    }
 
 
-		static public Location3D Origin { get { return new Location3D(); } }
-		public Length X { get; }
-		public Length Y { get; }
-		public Length Z { get; }
+    public static Location3D Origin { get { return new Location3D(); } }
+    public Length X { get; private set; }
+    public Length Y { get; private set; }
+    public Length Z { get; private set; }
 
 
-		static public Location3D From(UnitVector3D direction, Length magnitude)
-		{
-			return new Location3D(direction.X*magnitude, direction.Y*magnitude, direction.Z*magnitude);
-		}
-	}
+    public static Location3D From(UnitVector3D direction, Length magnitude)
+    {
+      return new Location3D(direction.X*magnitude, direction.Y*magnitude, direction.Z*magnitude);
+    }
+  }
 }
