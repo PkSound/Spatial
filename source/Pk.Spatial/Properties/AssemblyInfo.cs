b4952a8b0793aceb3c5cbdd874f6c59945dce31b
Jules LaPrairie <julesl@pksound.ca>
2016-10-13 11:14:16 -0600

    Added nuspec and trying to setup appveyor to handle versioning

--- source/Pk.Spatial/Properties/AssemblyInfo.cs
+++ source/Pk.Spatial/Properties/AssemblyInfo.cs
@@ -34,3 +34,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.0.0.0")]
 [assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.0" ) ]
