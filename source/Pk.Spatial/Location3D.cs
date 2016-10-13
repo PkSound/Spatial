9708afdf3976c2dc24c16f200fc09af246c10920
Jules LaPrairie <julesl@pksound.ca>
2016-10-13 05:08:49 -0600

    Starting to test drive Location3D

---
+++ source/Pk.Spatial/Location3D.cs
@@ -0,0 +1,31 @@
+﻿using MathNet.Spatial.Euclidean;
+using UnitsNet;
+
+namespace Pk.Spatial
+{
+	/// <summary>
+	///   Describes a location in 3D space.
+	///   X Y and Z each describe the distance from the origin in their respective axis.
+	/// </summary>
+	public struct Location3D
+	{
+		public Location3D(Length x, Length y, Length z)
+		{
+			this.X = x;
+			this.Y = y;
+			this.Z = z;
+		}
+
+
+		static public Location3D Origin { get { return new Location3D(); } }
+		public Length X { get; }
+		public Length Y { get; }
+		public Length Z { get; }
+
+
+		static public Location3D From(UnitVector3D direction, Length magnitude)
+		{
+			return new Location3D(direction.X*magnitude, direction.Y*magnitude, direction.Z*magnitude);
+		}
+	}
+}
